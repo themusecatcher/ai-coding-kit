7e56b30e7d2e236a68d65092f4a011e34ac5ad6b
themusecatcher <themusecatcher@163.com>
2026-09-15 14:27:09 +0800

    docs: deploy site

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.BtLzoY0m.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DRIV4PeM.js";import{_ as a}from"../app.BikW2mIf.js";import"./chunk-5VM5RSS4.DCWnmUUw.js";import"./chunk-XXDRQBXY.BnbGlxwE.js";import"./chunk-KBJHAD2P.C9xchXT_.js";import"./chunk-2GRJ4B5K.CKYYF8hA.js";import"./channel.DreSfX1G.js";import"./framework.BUwdTBzS.js";import"./theme.9-3t0WQs.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
